ed23bd24aa6a31abffd07168ea6d330a870dd46a
Christal Vigil <c.vigil27@gmail.com>
2020-10-17 20:54:01 -0600

    got functionality working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,6 @@
 // pulling in dependency 
 
-var fs = require("fs");
+var fs = require('fs');
 
 // creating a variable that will call on the myNotes function 
 var newNotes = myNotes();
@@ -25,30 +25,36 @@ function myNotes() {
 // GET/POST/DELETE CALLS 
 module.exports = ((app) => {
 
-    // this GET request will execute the function above and will render (in the form of an object), the notes added to the db.json file with unique IDs 
-    app.get("/api/notes", ((req, res) => {
+// this GET request will execute the function above and will render (in the form of an object), the notes added to the db.json file with unique IDs 
+    app.get('/api/notes', ((req, res) => {
         newNotes = myNotes();
         res.json(newNotes);
     }))   
 
-    // this POST request will allow the user to add a new note. The new note will be pushed to the existing list of notes in the db.json file. 
+// this POST request will allow the user to add a new note. The new note will be pushed to the existing list of notes in the db.json file. 
 
-    app.post("/api/notes", ((req, res) => {
+    app.post('/api/notes', ((req, res) => {
         newNotes.push(req.body);
         fs.writeFileSync('./Develop/db/db.json', JSON.stringify(newNotes), 'utf8');
         res.json(true);
     }))
 
-  // this DELETE request will allow the user to delete a note. 
+// this DELETE request will allow the user to delete a note. 
 
-    app.delete("/api/notes/:id", ((req, res) => {
-        var newID = req.params.id;
-        var noteNew = newNotes.filter(newNote => {
-            return noteNew.id === newID;
-        });
-        var indexSearch = newNotes.indexOf(noteNew);
-        newNotes.splice(indexSearch, 1);
-        fs.writeFileSync('./Develop/db/db.json', JSON.stringify(newNotes), 'utf8');
-        res.json("Your Note Has Been Deleted");
+  app.delete('/api/notes/:id',  ((req, res) => {
+
+    var ID = req.params.id;
+    
+    var noteDel = newNotes.filter(noteDel => {
+
+        return noteDel.id === ID; })[0];
+
+    var index = newNotes.indexOf(noteDel);
+
+    newNotes.splice(index, 1);
+
+    fs.writeFileSync('./Develop/db/db.json', JSON.stringify(newNotes), 'utf8');
+
+    res.json('This Note Has Been Deleted');
     }));
-});
+})
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,9 +1,7 @@
 // pull in path dependency 
-
 var path = require("path"); 
 
 // get request to return the notes.html file 
-
 module.exports = ((app) => {
   app.get("/notes", ((req, res) => {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
@@ -15,4 +13,4 @@ module.exports = ((app) => {
     res.sendFile(path.join(__dirname + "../public/index.html"));
     console.log("Successfully returned index.html")
   }))
-})
+});
